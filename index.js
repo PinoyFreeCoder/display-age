7f216d7b7dc85c8878d9731be01c050816d020af
pinoyfreecoder <pinoyfreecoder@gmail.com>
2024-01-09 09:42:00 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function calculateAge(birthday) {
+  const birthDate = new Date(birthday);
+  const currentDate = new Date();
+
+  let age = currentDate.getFullYear() - birthDate.getFullYear();
+
+  if (
+    currentDate.getMonth() < birthDate.getMonth() ||
+    (currentDate.getMonth() === birthDate.getMonth() &&
+      currentDate.getDate() < birthDate.getDate())
+  ) {
+    age--;
+  }
+
+  return age;
+}
+
+module.exports = calculateAge;
